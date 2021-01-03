70301ebaed8d704bfebdb608825611f1ba419537
SniKuz <ayyjh321@gmail.com>
2021-01-04 00:16:43 +0900

    All Skill Icons and Think about SKill effect(just think. not develop.) and make staff UI

--- Assets/Scripts/StreamerSkillManager.cs
+++ Assets/Scripts/StreamerSkillManager.cs
@@ -5,83 +5,96 @@ using UnityEngine;
 public class StreamerSkillManager : MonoBehaviour
 {
     public Sprite[] skillIcons; //Skill Icons. 여기다 넣어서 아이콘 바꿈.
-    public StreamerSkillVo selecetedSkill;
-    StreamerSkillVo[] skillGrid;
+    StreamerSkillVo[] skillList;
 
     
     private void Awake() {
-        skillGrid = new StreamerSkillVo[20];
+        skillList = new StreamerSkillVo[20];
         GenerateStreamerSkill();
     }
 
     public StreamerSkillVo GetSkill(int skillId){
-        return skillGrid[skillId];
+        return skillList[skillId];
+    }
+
+    public void SkillLevelUp(int id){
+        if(skillList[id]._level >=5) return;
+        skillList[id]._level++;
     }
 
     void GenerateStreamerSkill(){
-        StreamerSkillVo GoldSaHyang0 = new StreamerSkillVo(0,skillIcons[0], "치명적인 귀여움", 0, new int[5] {1000, 3000, 50000, 10000, 20000}, 
-        "시청자 수 증가량 + 10%",  "치명적인 귀여움을 뽑냅니다");
+        StreamerSkillVo GoldSaHyang0 = new StreamerSkillVo(0,skillIcons[0], "치명적인 귀여움", 0, new int[5] {1000, 3000, 5000, 10000, 20000}, 
+        new string[6] {"시청자 수 증가량 + 10%", "", "", "", "", ""},  "치명적인 귀여움을 뽑냅니다");
         StreamerSkillVo GoldSaHyang1 = new StreamerSkillVo(1,skillIcons[1], "질러팅", 0, new int[5] {1000, 3000, 50000, 10000, 20000}, 
-        "시청자 수, 도네 증가량 + 5%",  "질러팅을 진행할 확률이 증가합니다");
-        StreamerSkillVo GoldSaHyang2 = new StreamerSkillVo(2,skillIcons[2], "숏과 까미", 0, new int[5] {1000, 3000, 50000, 10000, 20000}, 
-        "아직 무슨 효과인지 밝혀지지 않았습니다",  "숏과 까미가 귀엽습니다."); // 막라 난이도 낮추는 용으로 괜찮은듯
-        StreamerSkillVo GoldSaHyang3 = new StreamerSkillVo(3,skillIcons[3], "굿즈생산", 0, new int[5] {1000, 3000, 50000, 10000, 20000}, 
-        "굿즈 생산 터치 수 감소",  "새로운 굿즈를 만듭니다.");
-        StreamerSkillVo GoldSaHyang4 = new StreamerSkillVo(4,skillIcons[4], "지모노X찍찍단", 0, new int[5] {1000, 3000, 50000, 10000, 20000}, 
-        "일정확률로 돈, 시청자 수가 증가합니다",  "지모노와 찍찍단이 단결합니다. 매 게이지마다 일정확률로 돈, 시청자 수가 증가합니다");
+        new string[6] {"시청자 수 증가량 + 10%", "", "", "", "",""},  "질러팅을 진행할 확률이 증가합니다");
+        StreamerSkillVo GoldSaHyang2 = new StreamerSkillVo(2,skillIcons[2], "숏과 까미", 0, new int[5] {1000, 3000, 5000, 10000, 20000}, 
+        new string[6] {"시청자 수 증가량 + 10%", "", "", "", "",""},  "숏과 까미가 귀엽습니다."); // 막라 난이도 낮추는 용으로 괜찮은듯
+        StreamerSkillVo GoldSaHyang3 = new StreamerSkillVo(3,skillIcons[3], "굿즈생산", 0, new int[5] {1000, 3000, 5000, 10000, 20000}, 
+        new string[6] {"시청자 수 증가량 + 10%", "", "", "", "",""},  "새로운 굿즈를 만듭니다.");
+        StreamerSkillVo GoldSaHyang4 = new StreamerSkillVo(4,skillIcons[4], "지모노", 0, new int[5] {1000, 3000, 5000, 10000, 20000}, 
+        new string[6] {"시청자 수 증가량 + 10%", "", "", "", "",""},  "지모노");
 
-        StreamerSkillVo Collet0 = new StreamerSkillVo(5,skillIcons[5], "C++ 개발자", 0, new int[5] {1000, 3000, 50000, 10000, 20000}, 
-        "시청자 수 증가량 + 10%",  "C++ 개발을 착수합니다");
-        StreamerSkillVo Collet1 = new StreamerSkillVo(6,skillIcons[6], "코정치", 0, new int[5] {1000, 3000, 50000, 10000, 20000}, 
-        "시청자 수 증가량 + 10%",  "훌륭한 정치력을 가지고있습니다. 이를 올바른데 사용하기 시작합니다.");
-        StreamerSkillVo Collet2 = new StreamerSkillVo(7,skillIcons[7], "샤샤", 0, new int[5] {1000, 3000, 50000, 10000, 20000}, 
-        "시청자 수 증가량 + 10%",  "샤샤를 모십니다.");
-        StreamerSkillVo Collet3 = new StreamerSkillVo(8,skillIcons[8], "애교", 0, new int[5] {1000, 3000, 50000, 10000, 20000}, 
-        "시청자 수 증가량 + 10%",  "방송에서 애교를 할 확률이 증가합니다");
-        StreamerSkillVo Collet4 = new StreamerSkillVo(9,skillIcons[9], "애교", 0, new int[5] {1000, 3000, 50000, 10000, 20000}, 
-        "시청자 수 증가량 + 10%",  "방송에서 애교를 할 확률이 증가합니다");
+        StreamerSkillVo Collet0 = new StreamerSkillVo(5,skillIcons[5], "C++ 개발자", 0, new int[5] {1000, 3000, 5000, 10000, 20000}, 
+        new string[6] {"터치 효율 증가 0->10%", "터치 효율 증가 10->20%", "터치 효율 증가 20->30%", "터치 효율 증가 30->40%", "터치 효율 증가 40->50%","터치 효율 증가 50%"},  
+        "C++ 개발을 착수합니다\n 굿즈 개발 기기의 효율이 올라갑니다.");
+        StreamerSkillVo Collet1 = new StreamerSkillVo(6,skillIcons[6], "코정치", 0, new int[5] {1000, 3000, 5000, 10000, 20000}, 
+        new string[6] {"모든 일반스킬 효율 0->1%", "모든 일반스킬 효율 1->2%", "모든 일반스킬 효율 2->3%", "모든 일반스킬 효율 3->4%", "모든 일반스킬 효율 4->5%","모든 일반스킬 효율 5%"},  
+        "훌륭한 정치력을 가지고있습니다.\n이를 올바른데 사용하기 시작합니다.");
+        StreamerSkillVo Collet2 = new StreamerSkillVo(7,skillIcons[7], "샤샤님과 합방", 0, new int[5] {1000, 3000, 0000, 10000, 20000}, 
+        new string[6] {"격주마다 시청자 수 + 0->100", "격주마다 시청자 수 + 100->200", "격주마다 시청자 수 + 200->300", "격주마다 시청자 수 + 300->400", "격주마다 시청자 수 + 400->500","격주마다 시청자 수 + 500"},  
+        "2주의 한 번씩 머기업 샤샤님과 합방을 진행합니다.");
+        StreamerSkillVo Collet3 = new StreamerSkillVo(8,skillIcons[8], "뱅", 0, new int[5] {1000, 3000, 5000, 10000, 20000}, 
+        new string[6] {"3% 확률로 0->1뱅 획득", "3% 확률로 1->2뱅 획득", "3% 확률로 2->3뱅 획득", "3% 확률로 3->4뱅 획득", "3% 확률로 4->5뱅 획득","3% 확률로 5뱅 획득"},  
+        "극악의 확률로 하루가 지날 때 레벨에 따른 뱅을 얻습니다.\n*1뱅은 3천5백만입니다.");//1뱅 = 35000000
+        StreamerSkillVo Collet4 = new StreamerSkillVo(9,skillIcons[9], "수숭과 줴자", 0, new int[5] {1000, 3000, 5000, 10000, 20000}, 
+       new string[6] {"시청자 수 증가량 + 10%", "", "", "", "",""},  "줴자는 수숭에게 가르침을 받습니다.");
 
-        StreamerSkillVo Tamx20 = new StreamerSkillVo(10,skillIcons[10], "나도 알고있어~ 내가.. 귀여운거..", 0, new int[5] {1000, 3000, 50000, 10000, 20000}, 
-        "매 게이지마다 골드 + 500",  "자신의 귀여움을 인정합니다. 미션을 성공합니다.");
-        StreamerSkillVo Tamx21 = new StreamerSkillVo(11,skillIcons[11], "하늘이 버린 게임실력", 0, new int[5] {1000, 3000, 50000, 10000, 20000}, 
-        "시청자 수 증가량 + 10%",  "놀라지 마세요. 그녀의 게임실력은 현실입니다.");
-        StreamerSkillVo Tamx22 = new StreamerSkillVo(12,skillIcons[12], "JMT", 0, new int[5] {1000, 3000, 50000, 10000, 20000}, 
-        "시청자 수 증가량 + 10%",  "비슷한 스킬이 있는것 같지만 신경쓰지 맙시다. 하나의 스킬로 표현하기 힘듭니다.");
-        StreamerSkillVo Tamx23 = new StreamerSkillVo(13,skillIcons[13], "탬동부 출동", 0, new int[5] {1000, 3000, 50000, 10000, 20000}, 
-        "시청자 수 증가량 + 10%",  "탬동부가 야근을 합니다.");
-        StreamerSkillVo Tamx24 = new StreamerSkillVo(14,skillIcons[14], "너무 커~버린 손녀", 0, new int[5] {1000, 3000, 50000, 10000, 20000}, 
-        "시청자 수 증가량 + 10%",  "그녀는 계속 커집니다. 아, 신장을 말하는게 아닙니다.");
+        StreamerSkillVo Tamx20 = new StreamerSkillVo(10,skillIcons[10], "나도 알아 내가 귀여운거", 0, new int[5] {1000, 3000, 5000, 10000, 20000}, 
+        new string[6] {"시청자 수 증가량 + 10%", "", "", "", "",""},  
+        "자신의 귀여움을 인정합니다. 미션을 성공합니다.");
+        StreamerSkillVo Tamx21 = new StreamerSkillVo(11,skillIcons[11], "밀감이", 0, new int[5] {1000, 3000, 5000, 10000, 20000}, 
+       new string[6] {"시청자 수 증가량 + 10%", "", "", "", "",""},  "밀감이를 보신적이 있으십니까?\n 그 촉감, 움직임, 당신은 헤어나오지 못할겁니다.");
+        StreamerSkillVo Tamx22 = new StreamerSkillVo(12,skillIcons[12], "JMT", 0, new int[5] {1000, 3000, 5000, 10000, 20000}, 
+        new string[6] {"모든 주식 하락률 0->1% 감소", "모든 주식 하락률 1->2% 감소", "모든 주식 하락률 2->3% 감소", "모든 주식 하락률 3->4% 감소", "모든 주식 하락률 4->5% 감소","모든 주식 하락률 5% 감소"},  
+        "DO you know JMT?\n그녀의 실력은 주식시장이 배려할 정도입니다.");
+        StreamerSkillVo Tamx23 = new StreamerSkillVo(13,skillIcons[13], "감사링~ 감사띠~", 0, new int[5] {1000, 3000, 5000, 10000, 20000}, 
+       new string[6] {"수금율 + 0->10%", "수금율 + 10->20%", "수금율 + 20->30%", "수금율 + 30->40%", "수금율 + 40->50%","수금율 + 50%"},  
+       "아이콘에 당황하셨죠?\n리액션을 열심히 합니다.");
+        StreamerSkillVo Tamx24 = new StreamerSkillVo(14,skillIcons[14], "너무 커~버린 손녀", 0, new int[5] {1000, 3000, 5000, 10000, 20000}, 
+        new string[6] {"시청자 수 증가량 + 10%", "", "", "", "",""},  "그녀는 계속 커집니다.\n 아, 신장을 말하는게 아닙니다.");
 
-        StreamerSkillVo Nanayang0 = new StreamerSkillVo(15,skillIcons[15], "나테이커 효과", 0, new int[5] {1000, 3000, 50000, 10000, 20000}, 
-        "시청자 수 증가량 + 10%",  "놀라지 마세요. 그녀의 게임실력 또한 현실입니다.");
-        StreamerSkillVo Nanayang1 = new StreamerSkillVo(16,skillIcons[16], "사장님", 0, new int[5] {1000, 3000, 50000, 10000, 20000}, 
-        "시청자 수 증가량 + 10%",  "사장님을 부릅니다.");
-        StreamerSkillVo Nanayang2 = new StreamerSkillVo(17,skillIcons[17], "Coke디렉터", 0, new int[5] {1000, 3000, 50000, 10000, 20000}, 
-        "시청자 수 증가량 + 10%",  "Coke 디렉터입니다. 응원과 존경을 보냅니다.");
-        StreamerSkillVo Nanayang3 = new StreamerSkillVo(18,skillIcons[18], "머찐나", 0, new int[5] {1000, 3000, 50000, 10000, 20000}, 
-        "시청자 수 증가량 + 10%",  "그녀는 머찝니다.");
-        StreamerSkillVo Nanayang4 = new StreamerSkillVo(19,skillIcons[19], "우주대스타", 0, new int[5] {1000, 3000, 50000, 10000, 20000}, 
-        "시청자 수 증가량 + 10%",  "우주에서 가장 빛나는 별입니다. 그녀를 모르는 우주인은 없을겁니다.");
+        StreamerSkillVo Nanayang0 = new StreamerSkillVo(15,skillIcons[15], "나테이커 효과", 0, new int[5] {1000, 3000, 5000, 10000, 20000}, 
+       new string[6] {"시청자 수 증가량 + 10%", "", "", "", "",""},  "놀라지 마세요.\n 그녀의 게임실력 또한 현실입니다.");
+        StreamerSkillVo Nanayang1 = new StreamerSkillVo(16,skillIcons[16], "사장님", 0, new int[5] {1000, 3000, 5000, 10000, 20000}, 
+       new string[6] {"모든 주식 상승률 0->1%", "모든 주식 상승률 1->2%", "모든 주식 상승률 2->3%", "모든 주식 상승률 3->4%", "모든 주식 상승률 4->5%","모든 주식 상승률 5%"},  
+       "사장님을 부릅니다.\n기업인의 힘을 보여주죠.");
+        StreamerSkillVo Nanayang2 = new StreamerSkillVo(17,skillIcons[17], "Coke디렉터", 0, new int[5] {1000, 3000, 5000, 10000, 20000}, 
+       new string[6] {"시청자 수 증가량 + 10%", "", "", "", "",""},  "Coke 디렉터입니다.\n 응원과 존경을 보냅니다.");
+        StreamerSkillVo Nanayang3 = new StreamerSkillVo(18,skillIcons[18], "머찐나", 0, new int[5] {1000, 3000, 5000, 10000, 20000}, 
+        new string[6] {"시청자 수 증가량 + 10%", "", "", "", "",""},  "그녀는 머찝니다.\n카리스마로 굿즈 디자인 효율이 증가합니다.");
+        StreamerSkillVo Nanayang4 = new StreamerSkillVo(19,skillIcons[19], "우주대스타", 0, new int[5] {1000, 3000, 5000, 10000, 20000}, 
+        new string[6] {"시청자 수 증가량 + 0->30%", "시청자 수 증가량 + 30->50%", "시청자 수 증가량 + 50->70%", "시청자 수 증가량 + 70->90%", "시청자 수 증가량 + 90->120%","시청자 수 증가량 + 120%"},  
+        "우주에서 가장 빛나는 별입니다.\n 그녀를 모르는 우주인은 없을겁니다.");
 
-        skillGrid[0] = GoldSaHyang0;
-        skillGrid[1] = GoldSaHyang1;
-        skillGrid[2] = GoldSaHyang2;
-        skillGrid[3] = GoldSaHyang3;
-        skillGrid[4] = GoldSaHyang4;
-        skillGrid[5] = Collet0;
-        skillGrid[6] = Collet1;
-        skillGrid[7] = Collet2;
-        skillGrid[8] = Collet3;
-        skillGrid[9] = Collet4;
-        skillGrid[10] = Tamx20;
-        skillGrid[11] = Tamx21;
-        skillGrid[12] = Tamx22;
-        skillGrid[13] = Tamx23;
-        skillGrid[14] = Tamx24;
-        skillGrid[15] = Nanayang0;
-        skillGrid[16] = Nanayang1;
-        skillGrid[17] = Nanayang2;
-        skillGrid[18] = Nanayang3;
-        skillGrid[19] = Nanayang4;
+        skillList[0] = GoldSaHyang0;
+        skillList[1] = GoldSaHyang1;
+        skillList[2] = GoldSaHyang2;
+        skillList[3] = GoldSaHyang3;
+        skillList[4] = GoldSaHyang4;
+        skillList[5] = Collet0;
+        skillList[6] = Collet1;
+        skillList[7] = Collet2;
+        skillList[8] = Collet3;
+        skillList[9] = Collet4;
+        skillList[10] = Tamx20;
+        skillList[11] = Tamx21;
+        skillList[12] = Tamx22;
+        skillList[13] = Tamx23;
+        skillList[14] = Tamx24;
+        skillList[15] = Nanayang0;
+        skillList[16] = Nanayang1;
+        skillList[17] = Nanayang2;
+        skillList[18] = Nanayang3;
+        skillList[19] = Nanayang4;
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -10,6 +10,7 @@ public class UIManager : MonoBehaviour
 	public GameManager gameManager;
 	public GoodsSystem goodsSystem;
     public StreamerSkillManager skillManager;
+    public StaffManager staffManager;
 
 	public Image dateImage;
 	public Sprite[] guageImage;
@@ -21,6 +22,7 @@ public class UIManager : MonoBehaviour
 
 	public RectTransform stockPanel;
 	public RectTransform streamerPanel;
+    public RectTransform staffPanel;
 
 	public GameObject GoldSaHyangUpgradeSet;
     public GameObject GoldSaHyangPanelBnt;
@@ -39,29 +41,60 @@ public class UIManager : MonoBehaviour
     public TextMeshProUGUI skillDescFunc;
     public TextMeshProUGUI skillDescIntroduce;
     public TextMeshProUGUI skillDescGold;
+    public StreamerSkillVo selectedSkill; //
+
+    public GameObject staffGetBtn;
+    public GameObject staffSetBtn;
+    public TextMeshProUGUI staffSInText;
+    public TextMeshProUGUI staffSOutText;
+    public TextMeshProUGUI staffAInText;
+    public TextMeshProUGUI staffAOutText;
+    public TextMeshProUGUI staffBInText;
+    public TextMeshProUGUI staffBOutText;
+    public TextMeshProUGUI staffCInText;
+    public TextMeshProUGUI staffCOutText;
+    public TextMeshProUGUI staffGoldPerGuage;
+    public TextMeshProUGUI staffCapacity;
 
     private void Start() {
-        for(int i = 0; i<20; i++){
-            StreamerSkillVo skill = skillManager.GetSkill(i);
-            skillTreeIcons[i].sprite = skill._icon;
-            skillTreeLevel[i].text = "Lv." + skill._level;
-        }
+
+        //#. SkillTree Icon Init
+        skillUIUpdate();
     }
 	private void LateUpdate() {
 
         //#0. 
 		dateImage.fillAmount = gameManager.time/20f;
-		dateText.text = gameManager.day+"" ;		
+		dateText.text = gameManager.day+"" ;	
+
+        //#.Money UI	
 		if(gameManager.money > 100000) moneyText.text = string.Format("{0:n0}", gameManager.money/1000)+"k";
 		else moneyText.text = string.Format("{0:n0}", gameManager.money);
+
+        //#.Goods UI
 		if(gameManager.goods > 100000) goodsText.text = gameManager.goods/1000 +"k/" + goodsSystem.maxGoodsCapacity/1000 +"k";
 		else goodsText.text = gameManager.goods + "/" + goodsSystem.maxGoodsCapacity;
+
+        //#.Viwer UI
 		if(gameManager.viwer > 100000) viwerText.text = string.Format("{0:n0}", gameManager.viwer/1000)+"k";
 		else viwerText.text = string.Format("{0:n0}", gameManager.viwer);
 
 		battery.gameObject.GetComponent<Image>().sprite = guageImage[5 - gameManager.guage]; // battery change
+
+        //#.Staff UI
+        staffSInText.text = string.Format("{0:n0}", staffManager.staffCnt[(int)gameManager.curType, 0]);
+        staffAInText.text = string.Format("{0:n0}", staffManager.staffCnt[(int)gameManager.curType, 1]);
+        staffBInText.text = string.Format("{0:n0}", staffManager.staffCnt[(int)gameManager.curType, 2]);
+        staffCInText.text = string.Format("{0:n0}", staffManager.staffCnt[(int)gameManager.curType, 3]);
+        staffSOutText.text = string.Format("{0:n0}", staffManager.waitStaffCnt[0]);
+        staffAOutText.text = string.Format("{0:n0}", staffManager.waitStaffCnt[1]);
+        staffBOutText.text = string.Format("{0:n0}", staffManager.waitStaffCnt[2]);
+        staffCOutText.text = string.Format("{0:n0}", staffManager.waitStaffCnt[3]);
+
+
 	}
 
+//------------------------------------------------[하단 Panle Btn]
 	public void PanelBtn(string panelName){
 		if(panelName == "StockPanel"){
 			if(stockPanel.localPosition.y == -878){
@@ -70,6 +103,9 @@ public class UIManager : MonoBehaviour
 				//#.Other UI Panel Hide
 				if(streamerPanel.localPosition.y == 0){
 					streamerPanel.DOLocalMoveY(-878, 0.4f);
+				}
+                if(staffPanel.localPosition.y == 0){
+					staffPanel.DOLocalMoveY(-878, 0.4f);
 				}
 			}else if(stockPanel.localPosition.y == 0){
 				stockPanel.DOLocalMoveY(-878, 0.4f).SetEase(Ease.InBack);
@@ -83,12 +119,31 @@ public class UIManager : MonoBehaviour
 				if(stockPanel.localPosition.y == 0){
 					stockPanel.DOLocalMoveY(-878, 0.4f);
 				}
+                if(staffPanel.localPosition.y == 0){
+					staffPanel.DOLocalMoveY(-878, 0.4f);
+				}
 			}else if(streamerPanel.localPosition.y == 0){
 				streamerPanel.DOLocalMoveY(-878, 0.4f).SetEase(Ease.InBack);
 			}
 		}
+        if(panelName == "StaffPanel"){
+			if(staffPanel.localPosition.y == -878){
+				staffPanel.DOLocalMoveY(0, 0.6f).SetEase(Ease.OutBack);
+
+				//#.Other UI Panel Hide
+				if(stockPanel.localPosition.y == 0){
+					stockPanel.DOLocalMoveY(-878, 0.4f);
+				}
+                if(streamerPanel.localPosition.y == 0){
+                    streamerPanel.DOLocalMoveY(-878, 0.4f).SetEase(Ease.InBack);
+                }
+			}else if(staffPanel.localPosition.y == 0){
+				staffPanel.DOLocalMoveY(-878, 0.4f).SetEase(Ease.InBack);
+			}
+		}
 	}
 
+    //------------------------------------[StreamerPanel TabBtn]
 	public void TabStreamerBtn(string streamerName){
 		switch (streamerName){
 			case "GoldSaHyang":
@@ -106,6 +161,8 @@ public class UIManager : MonoBehaviour
                 ColletPanelBtn.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
                 Tamx2PanelBtn.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
                 NanayangPanelBtn.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
+
+                SelectSkill(0);
 				break;
 			case "Collet":
                 GoldSaHyangUpgradeSet.SetActive(false);
@@ -122,6 +179,8 @@ public class UIManager : MonoBehaviour
                 ColletPanelBtn.GetComponentInChildren<TextMeshProUGUI>().color = new Color32(50, 50, 50, 255);
                 Tamx2PanelBtn.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
                 NanayangPanelBtn.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
+
+                SelectSkill(5);
 				break;
             case "Tamx2":
                 GoldSaHyangUpgradeSet.SetActive(false);
@@ -138,6 +197,8 @@ public class UIManager : MonoBehaviour
                 ColletPanelBtn.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
                 Tamx2PanelBtn.GetComponentInChildren<TextMeshProUGUI>().color = new Color32(50, 50, 50, 255);
                 NanayangPanelBtn.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
+
+                SelectSkill(10);
                 break;
 			case "Nanayang":
                 GoldSaHyangUpgradeSet.SetActive(false);
@@ -154,17 +215,86 @@ public class UIManager : MonoBehaviour
                 ColletPanelBtn.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
                 Tamx2PanelBtn.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
                 NanayangPanelBtn.GetComponentInChildren<TextMeshProUGUI>().color = new Color32(50, 50, 50, 255);
+
+                SelectSkill(15);
                 break;
 		}
 	}
 
+
+    //------------------------------------------[Streamer - Skill]
     public void SelectSkill(int id){
-        StreamerSkillVo skill = skillManager.GetSkill(id);
-        SkillDescImg.sprite = skill._icon;
-        skillDescName.text = skill._skillName;
-        skillDescLevel.text = "LV." + skill._level;
-        skillDescFunc.text = skill._function;
-        skillDescIntroduce.text = skill._skillIntroduce;
-        skillDescGold.text = "필요골드" + skill._nextLevelGold[skill._level];
+        selectedSkill = skillManager.GetSkill(id);
+        SkillDescImg.sprite = selectedSkill._icon;
+        skillDescName.text = selectedSkill._skillName;
+        skillDescLevel.text = "LV." + selectedSkill._level;
+        skillDescFunc.text = selectedSkill._function[selectedSkill._level];
+        skillDescIntroduce.text = selectedSkill._skillIntroduce;
+        skillDescGold.text = "필요골드" + selectedSkill._nextLevelGold[selectedSkill._level];
+        
     }
+
+    public void skillUIUpdate(){ //icon, SkillLevel Update
+        //#. SkillTree Init
+        for(int i = 0; i<20; i++){
+            StreamerSkillVo skill = skillManager.GetSkill(i);
+            skillTreeIcons[i].sprite = skill._icon;
+            skillTreeLevel[i].text = "Lv." + skill._level;
+        }
+    }
+
+
+    public void SkillUpBtn(){
+        if(selectedSkill._level <5){
+            gameManager.money -= selectedSkill._nextLevelGold[selectedSkill._level];
+            skillManager.SkillLevelUp(selectedSkill._id); // level++;
+            skillUIUpdate(); 
+
+            if(selectedSkill._level >=5){
+                skillDescLevel.text = "LV.MAX";
+                skillDescGold.text = "최대 레벨입니다.";
+                skillDescFunc.text = selectedSkill._function[5];
+            }else{
+                skillDescLevel.text = "LV." + selectedSkill._level;
+                skillDescGold.text = "필요골드" + selectedSkill._nextLevelGold[selectedSkill._level];
+                skillDescFunc.text = selectedSkill._function[selectedSkill._level];
+            }
+        }
+    }
+
+    //----------------------------------[StaffPanel]
+    public void TabStaffBtn(string staffGetSet){
+        switch(staffGetSet){
+            case "GetStaffPanel":
+
+                break;
+        }
+    }
+
+    public void StaffIn(string rank){
+        switch(rank){
+            case "S":
+                break;
+            case "A":
+                break;
+            case "B":
+                break;
+            case "C":
+                break;
+        }
+    }
+
+    public void StaffOut(string rank){
+        switch(rank){
+            case "S":
+                break;
+            case "A":
+                break;
+            case "B":
+                break;
+            case "C":
+                break;
+        }
+    }
+
 }
